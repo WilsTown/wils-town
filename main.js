d8579579132f4c71a3977c10d45accd135660c8d
Christian Quinzon <csquinzon@up.edu.ph>
2022-03-04 00:49:37 +0800

    icons+hover style

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ Vue.createApp({
     data() {
         return {
             display: '/assets/timer.PNG',
-            display_alt: 'town'
+            alt: 'town'
         }
     },
     methods: {
         viewTimer() {
             this.display = '/assets/timer.PNG',
-            this.display_alt = 'timer'
+            this.alt = 'timer'
         },
         viewTown() {
             this.display = '/assets/town.PNG',
-            this.display_alt = 'town'
+            this.alt = 'town'
         },
         viewEditTown() {
             this.display = '/assets/edittown.PNG',
-            this.display_alt = 'edittown'
+            this.alt = 'edittown'
         },
         viewSettings() {
             this.display = '/assets/settings.PNG',
-            this.display_alt = 'settings'
+            this.alt = 'settings'
         }
     },
 }).mount('#WT-app')
