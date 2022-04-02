8e7a55a5d01e1d6c2f16cf6ad733fcef3b2e59b8
Mutya <mcgulapa@up.edu.ph>
2022-04-02 15:48:33 +0800

    Vetur Default Tab Size [Sprint 3]

--- src/components/ViewTimer.vue
+++ src/components/ViewTimer.vue
@@ -1,46 +1,46 @@
 <template>
-  <div id="timer-container">
-    <div id="timer-component">
-      <TimerSession></TimerSession>
-      <StartButton
-        ButtonType="start-btn"
-        ButtonText="START"
-        ButtonState="none"
-      ></StartButton>
+    <div id="timer-container">
+        <div id="timer-component">
+            <TimerSession></TimerSession>
+            <StartButton
+                ButtonType="start-btn"
+                ButtonText="START"
+                ButtonState="none"
+            ></StartButton>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 import TimerSession from "./TimerSession";
 import StartButton from "./Button";
 export default {
-  name: "ViewTimer",
-  components: {
-    TimerSession,
-    StartButton,
-  },
+    name: "ViewTimer",
+    components: {
+        TimerSession,
+        StartButton,
+    },
 };
 </script>
 
 <style scoped>
 #timer-container {
-  position: absolute;
-  top: 40px;
-  bottom: 0%;
-  left: 0%;
-  right: 0%;
-  background-color: #e76f51;
+    position: absolute;
+    top: 40px;
+    bottom: 0%;
+    left: 0%;
+    right: 0%;
+    background-color: #e76f51;
 }
 
 #timer-component {
-  position: absolute;
-  left: 25%;
-  right: 25%;
-  top: 5%;
-  bottom: 20%;
+    position: absolute;
+    left: 25%;
+    right: 25%;
+    top: 5%;
+    bottom: 20%;
 
-  background: #af4b32;
-  border-radius: 25px;
+    background: #af4b32;
+    border-radius: 25px;
 }
 </style>
