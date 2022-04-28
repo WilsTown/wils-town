45d30a1494cc82f94d72bda7c6f523c310b7eeb5
Christian Quinzon <csquinzon@up.edu.ph>
2022-04-28 18:05:26 +0800

    Time input [Test 2]

--- src/components/TimeInput.vue
+++ src/components/TimeInput.vue
@@ -1,5 +1,5 @@
 <template>
-    <input type="tel" v-mask="['#:##','##:##', '###:##']" v-model="time_val" id="time">
+    <input type="tel" v-mask="['#:##','##:##', '###:##', '####:##']" v-model="time_val" id="time">
 </template>
 
 <script>
@@ -16,9 +16,11 @@ export default {
     watch: {
         time_val: {
             handler(val) {
-                if (val.length > 5) {
+                if (val.length > 6) {
                     this.time_val = this.time_val.slice(1,);
-                } else if (val.length == 5 && val.charAt(0)==0) {
+                } else if (val.length == 6 && val.slice(0,2) == "00") {
+                    this.time_val = this.time_val.slice(2,);
+                } else if (val.length == 5 && val.slice(0,1) == "0") {
                     this.time_val = this.time_val.slice(1,);
                 } else if (val.length < 4) {
                     this.time_val = "0".concat(this.time_val);
@@ -27,6 +29,9 @@ export default {
                 this.$emit('update', this.time_raw);
             }
         }
+    },
+    mounted() {
+        
     }
 };
 </script>
