94d63a115ed892fc1873c3d1f810f23f3442c5d0
Mutya <mcgulapa@up.edu.ph>
2022-04-28 23:27:07 +0800

    View Timer Default [Sprint 4]

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
     <div>
         <NavBar></NavBar>
-        <!-- <ViewTimer></ViewTimer> -->
-        <ViewTown></ViewTown>
+        <ViewTimer></ViewTimer>
+        <!-- <ViewTown></ViewTown> -->
     </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
-// import ViewTimer from "./components/ViewTimer.vue";
-import ViewTown from "./components/ViewTown.vue";
+import ViewTimer from "./components/ViewTimer.vue";
+// import ViewTown from "./components/ViewTown.vue";
 
 export default {
     name: "App",
     components: {
         NavBar,
-        // ViewTimer,
-        ViewTown,
+        ViewTimer,
+        // ViewTown,
     },
 };
 </script>
