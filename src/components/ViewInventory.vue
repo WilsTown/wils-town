af75e2db66375641a6e933e5d1b85949400953d7
annhipe <kdhipe@up.edu.ph>
2022-04-29 02:23:22 +0800

    Added ViewInventory

---
+++ src/components/ViewInventory.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="view-inventory-container">
+        <div id="left-inventory"> 
+            Inventory
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ViewInventory",
+    components: {},
+};
+</script>
+
+<style scoped>
+#view-inventory-container {
+    position: absolute;
+    display: flex;
+    top: 40px;
+    bottom: 0%;
+    left: 0%;
+    right: 0%;
+    background-color: #f4d35e;
+    justify-content: center;
+    align-items: center;
+}
+#left-inventory {
+    height: 945px;
+    width: 480px;
+    position: left;
+    top: 20px;
+    border-radius: 0px;
+    background-color: #607896;
+
+}
+</style>
