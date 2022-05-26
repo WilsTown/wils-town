0d787df5d827eaab0185e5792b671c6190368864
Mutya <mcgulapa@up.edu.ph>
2022-05-27 02:53:48 +0800

    Restored Edit Town UI Revisions [Improvements]

--- src/components/EditTown.vue
+++ src/components/EditTown.vue
@@ -1,28 +1,41 @@
 <template>
     <div id="edit-town">
-        <div v-if="StateEditing == 'enabled'" id="edit-buttons">
+        <div v-if="StateEditing == 'enabled'" id="edit-buttons-container">
             <EditButton
-                IconClass="fa fa-building"
+                IconClass="fa fa-wrench"
+                ButtonType="edit-town-btn"
                 @click="editBuild"
             ></EditButton>
             <EditButton
                 IconClass="fa fa-trash"
+                ButtonType="edit-town-btn"
                 @click="editRemove"
             ></EditButton>
-            <EditButton IconClass="fa fa-undo" @click="editUndo"></EditButton>
+            <EditButton
+                IconClass="fa fa-undo"
+                ButtonType="edit-town-btn"
+                @click="editUndo"
+            ></EditButton>
             <EditButton
                 IconClass="fa fa-undo"
                 style="transform: scaleX(-1)"
+                ButtonType="edit-town-btn"
                 @click="editRedo"
             ></EditButton>
-            <EditButton IconClass="fa fa-save" @click="saveTown"></EditButton>
+            <EditButton
+                IconClass="fa fa-save"
+                ButtonType="edit-town-btn"
+                @click="saveTown"
+            ></EditButton>
         </div>
-        <Plot
-            ref="Plot"
-            :StateEditing="StateEditing"
-            :SelectedElement="edit_state == 'build' ? SelectedElement : -1"
-            @changeLog="changeLog"
-        ></Plot>
+        <div id="plot-container">
+            <Plot
+                ref="Plot"
+                :StateEditing="StateEditing"
+                :SelectedElement="edit_state == 'build' ? SelectedElement : -1"
+                @changeLog="changeLog"
+            ></Plot>
+        </div>
     </div>
 </template>
 
@@ -103,20 +116,23 @@ export default {
 
 <style scoped>
 #edit-town {
-    position: fixed;
     top: 50px;
+    position: fixed;
     height: 90%;
     width: 69%;
-    background-color: #faf0ca;
+    background-color: #f3dfc2;
     border: none;
     border-radius: 5px;
 }
 
-#edit-buttons {
-    position: absolute;
-    left: 10px;
-    top: 10px;
-    width: 50px;
+#edit-buttons-container {
+    /* position: absolute; */
+    display: flex;
+    align-content: flex-start;
+    justify-content: flex-end;
+    align-self: center;
+    width: 98%;
     height: 50px;
+    margin: 10px;
 }
 </style>
