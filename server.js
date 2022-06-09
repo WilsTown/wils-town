ff61302362225fc7f87e5f1043102ee3b219ca3d
annhipe <kdhipe@up.edu.ph>
2022-06-09 17:50:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
-app.listen(port);
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Will be created later
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
